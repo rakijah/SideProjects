bb89fb2f23b7cf97726eee329116147a01f17131
rakijah <rakijah@gmx.de>
2017-05-23 21:07:58 +0200

    Minor fixes
    CSGOSquadButton:
    * Removed jQuery dependency because it was interfering with the default
    sites functionality

--- CSGOSquadButton/CSGOSquadButton.user.js
+++ CSGOSquadButton/CSGOSquadButton.user.js
@@ -1,18 +1,16 @@
 // ==UserScript==
 // @name         CSGOSquad Button
 // @namespace    rakijah
-// @version      0.2
+// @version      0.3
 // @description  Adds a CSGOSquad Button to Steam Profile pages
 // @author       rakijah
 // @grant        none
 // @include      /^https?:\/\/steamcommunity.com\/id\/[^\/]*\/?$/
 // @include      /^https?:\/\/steamcommunity\.com\/profiles\/([0-9]*)\/?$/
-// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js
 // ==/UserScript==
 /* jshint -W097 */
 'use strict';
-
-$(document).ready( function() {
+var createButton = function() {
     var p_id = new RegExp("^https?:\/\/steamcommunity.com\/id\/([^\/]*)\/?$");
     var p_profile = new RegExp("^https?:\/\/steamcommunity\.com\/profiles\/([0-9]*)\/?$");
     var url = window.location.href;
@@ -27,4 +25,14 @@ $(document).ready( function() {
     var button_html = '<a href="' + target + '" target="_blank" class="btn_profile_action btn_medium"><span>CS:GO Squad</span></a>';
     var element = document.getElementsByClassName("profile_header_actions")[0];
     element.insertAdjacentHTML('beforeend', button_html);
-});
+};
+
+(function() {
+    if (document.readyState == "complete" || document.readyState == "loaded" || document.readyState == "interactive") {
+        createButton();
+    } else {
+        document.addEventListener("DOMContentLoaded", function(event) {
+            createButton();
+        });
+    }
+})();
